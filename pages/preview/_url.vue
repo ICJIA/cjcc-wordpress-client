4f5c3413121c65aca5cfd862b59e0777f7bd85ac
cschweda <cschweda@gmail.com>
2018-10-22 11:33:16 -0500

    Test preview page

--- pages/preview/_url.vue
+++ pages/preview/_url.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
      
-       {{previewContentObject}}
+       <!-- {{previewContentObject}}
        <div v-if="error"><center>
          Error:<br>{{errMsg}}
          </center>
-         </div>
+         </div> -->
+         {{$route.query.url}}
         
     </div>
 </template>
@@ -13,18 +14,18 @@
 <script>
 import axios from '~/plugins/axios'
 export default {
-  async created() {
-    console.log('created')
-    try {
-      const { data } = await axios.get(this.$route.query.url)
-      this.previewContentObject = data
-      this.error = false
-    } catch (err) {
-      // console.error('Incorrect URL path')
-      // console.error(err)
-      this.errMsg = err.response
-    }
-  },
+  // async created() {
+  //   console.log('created')
+  //   try {
+  //     const { data } = await axios.get(this.$route.query.url)
+  //     this.previewContentObject = data
+  //     this.error = false
+  //   } catch (err) {
+  //     // console.error('Incorrect URL path')
+  //     // console.error(err)
+  //     this.errMsg = err.response
+  //   }
+  // },
   data() {
     return {
       previewContentObject: null,

c4dc632bfca6a9819c1a2fd0eb8071f22fc1b15d
Chris Schweda <cschweda@gmail.com>
2018-11-01 12:20:01 -0500

    Test with Algolia postgenerate script

---
+++ algolia.js
@@ -0,0 +1,97 @@
+let siteName = 'cjcc'
+
+require('dotenv').config()
+let jsonfile = require('jsonfile')
+let wtj = require('website-to-json')
+let trim = require('trim')
+let fs = require('fs')
+let algoliaIndex = []
+let parser = require('xml2json')
+let algoliasearch = require('algoliasearch')
+let crypto = require('crypto')
+let logger = require('./logger').createLogger()
+let previouslyIndexed
+
+const site = {
+  index: 'cjcc',
+  sitemap: 'https://cjcc.icjia.cloud/sitemap.xml',
+  urlBase: 'https://www.icjia.cloud/',
+  bodyContentId: '#page-content',
+  titleReplacement: 'ILLINOIS CJCC | '
+}
+
+appIndex = site.index
+sitemap = site.sitemap
+urlBase = site.urlBase
+titleReplacement = site.titleReplacement
+bodyContentId = site.bodyContentId
+
+// Get Sitemap from URL
+let request = require('sync-request')
+let res = request('GET', sitemap)
+let xml = res.getBody().toString('utf8')
+let myJson = parser.toJson(xml)
+let urls = JSON.parse(myJson)['urlset'].url
+let arrayOfUrls = urls.map(url => {
+  return url.loc
+})
+
+let num = arrayOfUrls.length
+
+function get(url) {
+  // create Algolia objectID based on hash of page url
+  let objectID = crypto
+    .createHash('md5')
+    .update(url)
+    .digest('hex')
+  console.log('Url: ', url)
+  return wtj.extractUrl(url, {
+    // create the page object for Algolia
+    fields: ['data'],
+    parse: function($) {
+      return {
+        keywords: $("meta[name='keywords' i]").attr('content'),
+        title: $('title')
+          .text()
+          .replace(titleReplacement, ''),
+        body: $('#page-content').text(),
+        description: $("meta[name='description' i]").attr('content'),
+        url: url.replace(/.*\/\/[^\/]*/, ''),
+        fullUrl: url,
+        objectID: objectID
+      }
+    }
+  })
+}
+
+arrayOfUrls.map(async function(url) {
+  await get(url).then(function(res) {
+    num--
+    algoliaIndex.push(res.data)
+    //console.log(res.data);
+    if (num === 0) {
+      // Finished generating
+      logger.info(`Finished querying URLs`)
+      setSearchObjects(algoliaIndex)
+    }
+  })
+})
+
+function setSearchObjects(objects) {
+  // Send pages to Algolia
+  let client = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_ADMIN_API_KEY
+  )
+
+  let index = client.initIndex(`${appIndex}`)
+  index.clearIndex(function(err, content) {
+    if (err) throw err
+    logger.info('Index cleared.')
+    logger.info(content)
+  })
+  index.addObjects(objects, function(err, content) {
+    if (err) throw err
+    logger.info('Indexing complete.')
+  })
+}

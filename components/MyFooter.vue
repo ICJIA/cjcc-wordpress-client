af57b8befd80a1746261c0fec7fa078ef9ea4472
cschweda <cschweda@gmail.com>
2019-01-02 13:53:05 -0600

    fix: adjust anchor styling in footer

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -5,18 +5,18 @@
       <v-flex xs12 sm5 hidden-sm-and-down offset-xs1>
         <div class="pt-5 pb-5">
           <h2 style="color: #fff; text-transform: uppercase;" class="pb-2">ICJIA Associate Sites</h2>
-          <div class="text-xs-left">
+          <div class="text-xs-left footerLink">
             <a href="http://www.icjia.org/redeploy/">Adult Redeploy Illinois</a>
           </div>
-          <div class="text-xs-left">
+          <div class="text-xs-left footerLink">
             <a
               href="http://www.icjia.state.il.us/ifvcc"
             >Illinois Family Violence Coordinating Councils</a>
           </div>
-          <div class="text-xs-left">
+          <div class="text-xs-left footerLink">
             <a href="https://ilheals.com/">Illinois HEALS</a>
           </div>
-          <div class="text-xs-left">
+          <div class="text-xs-left footerLink">
             <a href="https://illinoistvpp.org/">Targeted Violence Prevention Program</a>
           </div>
         </div>
@@ -34,14 +34,14 @@
               color="indigo darken-1"
               href="https://visitor.r20.constantcontact.com/manage/optin?v=001MqUcqqvjwLCJXlLMSWbTe3zHHmEQgFeBuHvBcJWTbwgrxFbDSGx4HSUPpI6DJWMUPgbljtLxffqIcGFTgCnr-auak88ybvRxpoJlTMGPtZs%3D"
             >Subscribe Now</v-btn>
-            <div class="infoLink pt-3">
+            <div class="tiny pt-3 footerLink">
               (312) 793-8550 - Fax: (312) 793-8422 -
               <a
                 href="mailto: cja.irc@illinois.gov"
               >cja.irc@illinois.gov</a>
             </div>
 
-            <div class="infoLink pt-2">
+            <div class="tiny pt-2 footerLink">
               <a
                 href="http://www.icjia.state.il.us"
               >© 2019 Illinois Criminal Justice Information Authority</a>
@@ -74,15 +74,15 @@ export default {
   font-size: 14px;
 }
 
-.infoLink {
+.tiny {
   font-size: 12px;
 }
 
-a {
+.footerLink a {
   color: #5c6bc0 !important;
 }
 
-a:hover {
+.footerLink a:hover {
   color: #777 !important;
 }
 
